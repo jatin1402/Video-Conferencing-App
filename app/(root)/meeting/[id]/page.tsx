b379c9e2bf5806fe424ce514b2e832b7bc0047c4
Jatin <jvarshney@plansource.com>
2025-03-16 23:21:19 +0530

    fixing Pageprops type error

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -6,16 +6,23 @@ import MeetingSetup from '@/components/MeetingSetup';
 import { useGetCallById } from '@/hooks/useGetCallById';
 import { useUser } from '@clerk/nextjs';
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { useParams } from 'next/navigation';
 import React, { useState } from 'react'
 
-const Meeting = ({ params }: { params: {id: string} }) => {
+const Meeting = () => {
 
-  const {isLoaded} = useUser();
+  const { id } = useParams();
+  const {isLoaded, user} = useUser();
   const [isSetupCompleted, setIsSetupCompleted] = useState(false);
 
-  const {call, callIsLoading} = useGetCallById(params?.id);
+  const {call, callIsLoading} = useGetCallById(id!);
 
   if(!isLoaded || callIsLoading) return <Loader />
+
+  if(!call) return <div className='text-3xl text-white font-bold text-center'>Call Not Found</div>
+
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m)=> {m.user_id === user.id}));
+  if(notAllowed) return <div className='text-3xl text-white font-bold text-center'>You are not allowed to join the call</div>
 
   return (
     <main className='h-screen w-full'>
